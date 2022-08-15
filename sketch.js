50bcb1fecc028ae7880fe9d779ab35be3588fc5f
Guardian57 <67869701+Guardian57@users.noreply.github.com>
2022-08-15 15:47:02 -0400

    inner height/width

--- sketch.js
+++ sketch.js
@@ -82,8 +82,8 @@ var scaleFactor;
 
 
 function setup() {
-  var cHeight = windowHeight; //height of the window
-  var cWidth = windowHeight * (1080/1920); 
+  var cHeight = innerHeight; //height of the window
+  var cWidth = innerWidth * (1080/1920); 
   scaleFactor = cHeight/1920;
   
   
